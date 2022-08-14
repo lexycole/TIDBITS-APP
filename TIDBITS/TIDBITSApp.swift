3c171a2eb335b9902d8f60906cb824e5cdafd4c1
Olawale Akinro <akinbolawa90@gmail.com>
2022-08-14 09:36:14 -0700

    Initial Commit

---
+++ TIDBITS/TIDBITSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TIDBITSApp.swift
+//  TIDBITS
+//
+//  Created by swift-rn-flutter on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TIDBITSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
